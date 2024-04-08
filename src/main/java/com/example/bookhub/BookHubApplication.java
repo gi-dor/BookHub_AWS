bd44ae66a6ea7fe2d0fdf78a22839cc94eeb817f
lucy <lucy1287@naver.com>
2024-04-08 17:23:34 +0900

    [Chore] 프로젝트 초기 설정

---
+++ src/main/java/com/example/bookhub/BookHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookhub/BookHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
