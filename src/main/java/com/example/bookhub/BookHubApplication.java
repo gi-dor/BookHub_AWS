a5a499bd2c829f983c6b4e6737e7fd646c4c3fc7
rbska <rbska1234@naver.com>
2024-04-08 17:37:00 +0900

    [test]브랜치 테스트

--- src/main/java/com/example/bookhub/BookHubApplication.java
+++ src/main/java/com/example/bookhub/BookHubApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookHubApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("hello world");
         SpringApplication.run(BookHubApplication.class, args);
     }
 
