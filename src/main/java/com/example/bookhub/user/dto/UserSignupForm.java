9e8051df6eb949f6e9f7dc5d5cb7abdc05bbe5fe
gi-dor <rltjs3563@naver.com>
2024-04-12 15:43:17 +0900

    Fix : NotBlank 제거
    NotBlank 사용시 , 공백란 을 입력 하라는 유효성 메세지 발생
    메세지가 너무 많음 , 공간 차지함

--- src/main/java/com/example/bookhub/user/dto/UserSignupForm.java
+++ src/main/java/com/example/bookhub/user/dto/UserSignupForm.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,27 +21,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class UserSignupForm {
 // 회원 가입 양식을 처리한다 , 사용자 등록할 때 쓰인다
 
-    @NotBlank(message = "ID는 필수 입력값 입니다")
-    @Pattern(regexp = "^[a-z0-9]{5,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 5~20자리여야 합니다.")
+
+    @Pattern(regexp = "^[a-z0-9]{5,20}$" , message ="알맞은 형식이 아닙니다")    // message = "아이디는 영어 소문자와 숫자만 사용하여 5~20자리여야 합니다."
     private String id;
 
-    @NotBlank(message= "비밀번호는 필수 입력 값 입니디")
-    @Size(min = 8 , message = "비밀번호는 8글자 이상 입니다")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$"  , message ="알맞은 형식이 아닙니다")
     private String password;
 
     // @Pattern(regexp = "$expression") 정규식검증
-    @NotBlank(message = "이름은 필수 입력 값입니다")
-    @Pattern(regexp="^[가-힣]{2,}$", message = "이름은 한글 2글자 이상")
+    @Pattern(regexp="^[가-힣]{2,}$" ,  message ="알맞은 형식이 아닙니다")
     private String name;
 
-    @NotBlank(message="이메일은 필수 입력 값입니다")
+
     @Email(message = "유효한 이메일 형식이 아닙니다")
     private String email;
 
-    @NotBlank(message = "전화번호는 필수 입력 값입니다")
-    @Pattern(regexp="^\\d{2,3}-\\d{3,4}-\\d{4}$" , message = "유효한 전화번호 형식이 아닙니다")
-    private String tel;
+
+   // @Pattern(regexp="^\\d{2,3}-\\d{3,4}-\\d{4}$" , message = "유효한 전화번호 형식이 아닙니다")
+    private  String tel;
 
     private String zipCode;
     private String address;
