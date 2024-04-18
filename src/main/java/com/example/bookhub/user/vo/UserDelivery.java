9f43b8ca35323493eb831f88c9f5bf364a218b19
gi-dor <rltjs3563@naver.com>
2024-04-18 17:04:39 +0900

    경로

---
+++ src/main/java/com/example/bookhub/user/vo/UserDelivery.java
@@ -0,0 +1,24 @@
+package com.example.bookhub.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDelivery {
+
+    private Long no;
+    private String name;
+    private String tel;
+    private String zipCode;
+    private String address;
+    private String addressDetail;
+    private String defaultAddressYn;
+    private User user;
+}
