ab1c64057dbafb8d4f5b1327fb878f0fbe122b41
lucy1287 <lucy1287@naver.com>
2024-04-18 11:02:11 +0900

    Feat: @ModelAttribute로 BuyForm을 세션에 저장

--- src/main/java/com/example/bookhub/product/controller/KakaoPayController.java
+++ src/main/java/com/example/bookhub/product/controller/KakaoPayController.java
@@ -1,19 +1,18 @@
 package com.example.bookhub.product.controller;
 
+import com.example.bookhub.product.dto.BuyForm;
 import com.example.bookhub.product.dto.KakaoApproveResponse;
 import com.example.bookhub.product.service.KakaoPayService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 @RequestMapping("/kakaoPay")
 @RequiredArgsConstructor
+@SessionAttributes({"buyForm"})
 public class KakaoPayController {
 
     private final KakaoPayService kakaoPayService;
@@ -22,16 +21,17 @@ public class KakaoPayController {
      * 결제요청
      */
     @PostMapping("/ready")
-    public String kakaoPayReady() {
-
+    public String kakaoPayReady(@ModelAttribute BuyForm buyForm) {
+        //System.out.println(buyForm);
         return "redirect:" + kakaoPayService.kakaoPayReady();
     }
 
     @GetMapping("/success")
-    public String kakaoPaySuccess(@RequestParam("pg_token")String pgToken, Model model) {
+    public String kakaoPaySuccess(@ModelAttribute BuyForm buyForm, @RequestParam("pg_token")String pgToken, Model model) {
         KakaoApproveResponse kakaoApprove = kakaoPayService.approveResponse(pgToken);
         System.out.println(kakaoApprove.toString());
         System.out.println(kakaoApprove.getAmount().toString());
+        System.out.println(buyForm);
         return "product/pay/success";
     }
 
