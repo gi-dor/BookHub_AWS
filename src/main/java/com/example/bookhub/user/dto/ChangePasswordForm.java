62334ab27a0b2b4c53ba2bc27b3a30b0ee0c51b8
gi-dor <rltjs3563@naver.com>
2024-04-24 17:19:27 +0900

    Feat : 비밀번호변경 DTO

---
+++ src/main/java/com/example/bookhub/user/dto/ChangePasswordForm.java
@@ -0,0 +1,25 @@
+package com.example.bookhub.user.dto;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordForm {
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$"  , message ="알맞은 형식이 아닙니다")
+    private String password;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$"  , message ="알맞은 형식이 아닙니다")
+    private String changePassword;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$"  , message ="알맞은 형식이 아닙니다")
+    private String confirmNewPassword;
+}
