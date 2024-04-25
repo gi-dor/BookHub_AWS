c20352f93ded386eb1110783dc2f1f380a7cf096
imkkkyeong <dlwldmsgo808@gmail.com>
2024-04-25 11:55:55 +0900

    Fix:검색기능(js코드 이동)

--- src/main/resources/static/js/main/searchList.js
+++ src/main/resources/static/js/main/searchList.js
@@ -12,10 +12,23 @@ $(document).ready(function () {
 });
 
 //판매량순, 출시일순, 평점순, 리뷰순
-function submitForm(sortBy) {
-    document.getElementById('sortForm').action = '/search?sort=' + encodeURIComponent(sortBy);
-    document.getElementById('sortForm').submit('');
-}
+    function submitExtra() {
 
+    $("#form-extra-search input[name=page]").val(1);
+    $("#form-extra-search input[name=sort]").val("sales");
+    $("#form-extra-search").trigger("submit");
+}
+    $(".searchSort a ").click(function (event) {
+    event.preventDefault();
+    $("#form-extra-search input[name=page]").val(1);
+    $("#form-extra-search input[name=sort]").val($(this).attr("data-sort"));
+    $("#form-extra-search").trigger("submit");
+});
+
+    $(".pagination a").click(function(event) {
+    event.preventDefault();
+    $("#form-extra-search input[name=page]").val($(this).attr("data-page"));
+    $("#form-extra-search").trigger("submit");
+})
 
 
