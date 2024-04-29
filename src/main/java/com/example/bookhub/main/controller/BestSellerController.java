a73fec284465bc1644c007e0401bdcbef554121d
imkkkyeong <dlwldmsgo808@gmail.com>
2024-04-29 17:06:34 +0900

    Feat:신간도서리스트 오류, 베스트셀러 미완

--- src/main/java/com/example/bookhub/main/controller/BestSellerController.java
+++ src/main/java/com/example/bookhub/main/controller/BestSellerController.java
@@ -2,6 +2,7 @@ package com.example.bookhub.main.controller;
 
 import com.example.bookhub.main.dto.BookListDto;
 import com.example.bookhub.main.dto.SearchCriteria;
+import com.example.bookhub.main.service.BestSellerService;
 import com.example.bookhub.main.service.NewBookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,30 +12,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
-public class NewBookController {
+public class BestSellerController {
 
 
-    private final NewBookService newBookService;
+    private final BestSellerService bestSellerService;
 
-    @GetMapping("/main/newbook")
+    @GetMapping("/main/bestseller")
     public String newBook(
-            @RequestParam(name="cate", required=false, defaultValue= "1") int cate,
             @RequestParam(name="cateKeyword", required=false, defaultValue= "0") int cateKeyword,
-            @RequestParam(name="pubDate", required=false, defaultValue= "1") int pubDate,
+            @RequestParam(name="pubDate", required=false, defaultValue= "0") int pubDate,
             @RequestParam(name="sort", required=false, defaultValue= "1") int sort,
             @RequestParam(name="page", required=false, defaultValue= "1") int page,
-
             Model model) { // Model 객체를 메서드 파라미터로 추가
         SearchCriteria searchCriteria = new SearchCriteria(); // 검색 조건 생성
-        searchCriteria.setCate(cate);
         searchCriteria.setCateKeyword(cateKeyword);
         searchCriteria.setPubDate(pubDate);
         searchCriteria.setSort(sort);
         searchCriteria.setPage(page)
         ;
-        BookListDto dto = newBookService.newBooks(searchCriteria); // NewBookService의 인스턴스를 통해 newBooks 메서드 호출
+        BookListDto dto = bestSellerService.bestSeller(searchCriteria);
         model.addAttribute("books", dto.getBooks()); // 모델에 책 정보를 담아서 HTML로 전달
         model.addAttribute("criteria", dto.getCriteria());
-        return "main/newList.html";
+        return "/main/bestseller.html";
     }
     }
--- src/main/java/com/example/bookhub/main/dto/BookDto.java
+++ src/main/java/com/example/bookhub/main/dto/BookDto.java
@@ -3,6 +3,7 @@ package com.example.bookhub.main.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
 import java.util.List;
 @Getter
 @Setter
