09f25358d5dbca3d552cfa502ff7a56cc298e4ca
lucy1287 <lucy1287@naver.com>
2024-05-07 09:57:54 +0900

    Feat: 도서상세 - 저자, 옮긴이, 출판사 표시

--- src/main/java/com/example/bookhub/product/mapper/BookMapper.java
+++ src/main/java/com/example/bookhub/product/mapper/BookMapper.java
@@ -1,7 +1,10 @@
 package com.example.bookhub.product.mapper;
 
 import com.example.bookhub.product.dto.BookDto;
+import com.example.bookhub.product.vo.BookAuthor;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BookMapper {
@@ -9,4 +12,5 @@ public interface BookMapper {
     BookDto getBookDetailByNo(long bookNo);
     BookDto getBookByBookNo(long bookNo);
     void updateBookStatus(long bookNo);
+    List<BookAuthor> getAuthorByBookNo(long bookNo);
 }
