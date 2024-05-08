77d397b07a78f961c12a56eff5e43784646ec90b
gi-dor <rltjs3563@naver.com>
2024-05-08 11:27:15 +0900

    Comment : 비동기 주석

--- src/main/java/com/example/bookhub/config/AsyncConfig.java
+++ src/main/java/com/example/bookhub/config/AsyncConfig.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@EnableAsync
+@EnableAsync    // 스프링에 비동기처리 활성화
 @Configuration
 public class AsyncConfig {
 
     @Bean
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(30);
-        executor.setQueueCapacity(50);
-        executor.initialize();
+        executor.setCorePoolSize(5);    // 스레드 풀의 기본크기
+        executor.setMaxPoolSize(30);    // 스레드 풀의 최대 크기
+        executor.setQueueCapacity(50);  // 작업 대기열의 용량 설정 , 대기열이 가득차면 추가 작업 거부
+        executor.initialize();          // 작업 완료후 스레드 풀 초기화
         return executor;
     }
 }
