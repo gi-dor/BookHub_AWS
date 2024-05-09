b4588700ffebfad1f724fb22f16748153ff8ab28
Useodam <db970212@gmail.com>
2024-05-09 17:27:44 +0900

    Refactor. DashBoardScheduleService Retry 로직 추가

--- src/main/java/com/example/bookhub/admin/service/DashBoardScheduleService.java
+++ src/main/java/com/example/bookhub/admin/service/DashBoardScheduleService.java
@@ -2,13 +2,10 @@ package com.example.bookhub.admin.service;
 
 import com.example.bookhub.admin.mapper.DashBoardScheduleMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
@@ -26,18 +23,44 @@ public class DashBoardScheduleService {
     @SchedulerLock(name = "yesterday_lock", lockAtLeastFor = "20s", lockAtMostFor = "50s")
     @Scheduled(cron = "0 0 10 * * ?")
     public void saveTotalYesterday() {
-        dashBoardScheduleMapper.saveTotalYesterday();
+        retryLogic(dashBoardScheduleMapper::saveTotalYesterday);
     }
 
     // 매주 월요일 10시마다 지난주 값을 계산하여 저장하기
     @SchedulerLock(name = "lastWeek_lock", lockAtLeastFor = "20s", lockAtMostFor = "50s")
     @Scheduled(cron = "0 00 10 * * MON")
-    public void saveTotalLastWeek() {dashBoardScheduleMapper.saveTotalLastWeek();}
+    public void saveTotalLastWeek() {
+        retryLogic(dashBoardScheduleMapper::saveTotalYesterday);
+    }
 
     // 매월 1일 오전 12시마다 지난달 값을 계산하여 저장하기
     @SchedulerLock(name = "lastMonth_lock", lockAtLeastFor = "20s", lockAtMostFor = "50s")
     @Scheduled(cron = "0 0 12 1 * ?")
     public void saveTotalLastMonth(){
-        dashBoardScheduleMapper.saveTotalLastMonth();
+        retryLogic(dashBoardScheduleMapper::saveTotalLastMonth);
+    }
+
+    private void retryLogic(Runnable task) {
+        int maxAttempts = 3; // 최대 시도 횟수
+        int attempts = 0;
+        boolean success = false;
+
+        while (attempts < maxAttempts) {
+            try {
+                // 작업 실행하고 작업이 성공하면 while문 탈출
+                task.run();
+                success = true;
+                break;
+            } catch (Exception ex){
+                attempts ++;
+                System.out.println("작업을 재시도합니다. 현재 시도횟수 : " + attempts);
+            }
+        }
+
+        if(!success) {
+            // 최대 시도 횟수를 넘겼을 때 실행된다
+            System.out.println("작업을 최대 시도 횟수를 초과하여 실패했습니다");
+
+        }
     }
 }
