a2e782c06e2e9ce9aa933b43c130e55ae542fdda
lucy1287 <lucy1287@naver.com>
2024-05-11 19:18:49 +0900

    Feat: DB 이중화 적용

--- src/test/java/com/example/bookhub/product/service/BookStockTest.java
+++ src/test/java/com/example/bookhub/product/service/BookStockTest.java
@@ -25,18 +25,18 @@ public class BookStockTest {
 
     @Test
     public void testCounterWithConcurrency() throws InterruptedException {
-        int numberOfThreads = 11;
+        int numberOfThreads = 6;
         ExecutorService service = Executors.newFixedThreadPool(10); // 스레드 풀을 생성
         CountDownLatch latch = new CountDownLatch(numberOfThreads); // 스레드의 완료를 추적
 
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.println("i : "+i);
             // service.execute()는 작업을 스레드 풀에 제출하고, 해당 작업이 완료될 때까지 기다리지 않고 다음 코드로 진행
             // 이후 작업은 백그라운드에서 별도의 스레드에서 실행
 
             service.execute(() -> { //  메서드를 호출할 때마다 새로운 작업이 스레드 풀에서 실행
                 BuyForm buyForm = new BuyForm();
-                buyForm.setBuyBookNoList(Arrays.asList(9791196222702L, 9791196222705L));
+                buyForm.setBuyBookNoList(Arrays.asList(9791196222695L, 9791196222696L));
                 buyForm.setBuyBookCountList(Arrays.asList(1, 1));
                 buyService.updateBookStock(buyForm);
                 latch.countDown();
@@ -45,9 +45,9 @@ public class BookStockTest {
         service.shutdown();
 
         latch.await(); // 모든 작업이 완료될 때까지 대기합니다.
-        System.out.println("now stock :"+ bookService.getBookByBookNo(9791196222702L).getStock());
+        System.out.println("now stock :"+ bookService.getBookByBookNo(9791196222695L).getStock());
 
-        assertEquals(7, bookService.getBookByBookNo(9791196222702L).getStock());
-        assertEquals(5534, bookService.getBookByBookNo(9791196222705L).getStock());
+        assertEquals(0, bookService.getBookByBookNo(9791196222695L).getStock());
+        assertEquals(0, bookService.getBookByBookNo(9791196222696L).getStock());
     }
 }
